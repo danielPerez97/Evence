66773165bbf5f5a75ceec5da7b101b57e0b5c7c1
doperez <doperez@uark.edu>
2019-11-04 07:58:46 -0600

    Set my MainViewModel

--- app/src/main/java/projects/csce/evence/di/AppComponent.java
+++ app/src/main/java/projects/csce/evence/di/AppComponent.java
@@ -5,8 +5,7 @@ import javax.inject.Singleton;
 import dagger.Component;
 import projects.csce.evence.di.loggedin.LoggedInModule;
 import projects.csce.evence.di.loggedin.LoggedInSubComponent;
-import projects.csce.evence.view.MainActivity;
-import projects.csce.evence.view.SecondActivity;
+import projects.csce.evence.view.main.MainActivity;
 
 @Singleton
 @Component(modules = {NetworkModule.class, LoginModule.class})
--- app/src/main/java/projects/csce/evence/view/main/MainActivity.java
+++ app/src/main/java/projects/csce/evence/view/main/MainActivity.java
@@ -1,4 +1,4 @@
-package projects.csce.evence.view;
+package projects.csce.evence.view.main;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +7,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -22,11 +23,15 @@ import projects.csce.evence.R;
 import projects.csce.evence.base.BaseApplication;
 import projects.csce.evence.databinding.ActivityMainBinding;
 import projects.csce.evence.utils.Utils;
+import projects.csce.evence.view.GenerateQR;
+import projects.csce.evence.view.SecondActivity;
 
 public class MainActivity extends AppCompatActivity
 {
 
     private int RC_SIGN_IN = 1;
+    private MainViewModel viewModel;
+    private CardsAdapter adapter;
     @Inject GoogleSignInClient signInClient;
 
     @Override
@@ -37,6 +42,8 @@ public class MainActivity extends AppCompatActivity
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setView(this);
         ButterKnife.bind(this);
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        viewModel.getCardData().observe(this, data -> adapter.setData(data));
     }
 
     public void startQrActivity()
