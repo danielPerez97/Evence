a3d28025ea5b612ed67436e423fc164bef6699f9
Gabriel <gddelcar@uark.edu>
2019-11-08 20:47:56 -0600

    UI changes. Updated the MainActivity to handle the RecyclerView. Updated RecyclerView Adapter. Created some dummy data to populate the RecyclerView. Todo: customize toolbar. Place signin option inside toolbar.fix fab

--- app/src/main/java/projects/csce/evence/viewmodel/MainViewModel.java
+++ app/src/main/java/projects/csce/evence/viewmodel/MainViewModel.java
@@ -4,14 +4,34 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import projects.csce.evence.service.model.Event;
 
 public class MainViewModel extends ViewModel
 {
+    private LiveData<List<Event>> eventsList;
+
     public LiveData<List<Event>> getCardData()
     {
         return new MutableLiveData<>();
     }
+
+    public LiveData<List<Event>> getEventsList()
+    {
+        return eventsList;
+    }
+
+    //for testing purposes. to populate the RecyclerView with a list of dummy data
+    public List<Event> generateDummyData(){
+       List<Event> dummyEvents = new ArrayList<>();
+       dummyEvents.add(new Event());
+       dummyEvents.add(new Event());
+       dummyEvents.add(new Event());
+       dummyEvents.add(new Event());
+       dummyEvents.add(new Event());
+       return dummyEvents;
+    }
+
 }
