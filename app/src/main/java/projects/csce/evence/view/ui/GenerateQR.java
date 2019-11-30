f0d39f7d847047c456be02a6be5b5c7dc45f8cca
danielPerez97 <doperez1997@yahoo.com>
2019-11-30 16:55:57 -0600

    Set up the Storage Access Framework to allow the user to save their .ics files.

--- app/src/main/java/projects/csce/evence/view/ui/GenerateQR.java
+++ app/src/main/java/projects/csce/evence/view/ui/GenerateQR.java
@@ -1,22 +1,33 @@
 package projects.csce.evence.view.ui;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
 import javax.inject.Inject;
 
+import okio.BufferedSink;
+import okio.Okio;
 import projects.csce.evence.R;
 import projects.csce.evence.databinding.ActivityGenerateQrBinding;
 import projects.csce.evence.ical.EventSpec;
 import projects.csce.evence.ical.ICalSpec;
-import projects.csce.evence.service.model.event.Event;
 import projects.csce.evence.service.model.qr.QrAttempt;
 import projects.csce.evence.utils.Utils;
 import projects.csce.evence.viewmodel.GenerateQrViewModel;
@@ -25,7 +36,7 @@ public class GenerateQR extends AppCompatActivity implements Observer<QrAttempt>
 {
     private GenerateQrViewModel viewModel;
     private ActivityGenerateQrBinding binding;
-    private Event currentEvent = new Event.Builder().build();
+    private ICalSpec currentEvent;
     @Inject ViewModelProvider.Factory viewModelFactory;
 
 
@@ -53,7 +64,7 @@ public class GenerateQR extends AppCompatActivity implements Observer<QrAttempt>
             {
                 Throwable e = ((QrAttempt.Failure) attempt).getE();
                 e.printStackTrace();
-                Utils.toastShort(getApplicationContext(), e.getLocalizedMessage());
+                Utils.toastShort(getApplicationContext(), Objects.requireNonNull(e.getLocalizedMessage()));
             }
     }
 
@@ -69,13 +80,59 @@ public class GenerateQR extends AppCompatActivity implements Observer<QrAttempt>
                 .end(Utils.toZonedDateTime(endMonthDayYear))
                 .build();
 
-        ICalSpec ical = ICalSpec.builder()
+        currentEvent = ICalSpec.builder()
                 .fileName(binding.titleEditText.getText().toString())
                 .addEvent(event)
                 .build();
 
         // Write the file to the file system
-        viewModel.saveFile(ical);
+        viewModel.saveFile(currentEvent);
+
+        // Bug the user about storing it
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/*");
+        intent.putExtra(Intent.EXTRA_TITLE, currentEvent.getFileName() + ".ics");
+        startActivityForResult(intent, 1);
+
+    }
+
+    // Handle the user choosing a place to store the file
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            Uri uri;
+            if (data != null) {
+                uri = data.getData();
+                try
+                {
+                    ParcelFileDescriptor pfd = getContentResolver().openFileDescriptor(uri, "w");
+
+                    FileOutputStream fileOutputStream = new FileOutputStream(pfd.getFileDescriptor());
+                    try(BufferedSink sink = Okio.buffer(Okio.sink(fileOutputStream)))
+                    {
+                        sink.writeUtf8(currentEvent.text());
+                    }
+                    catch (IOException e)
+                    {
+                        Log.i("FILEWRITER", e.getMessage());
+                        e.printStackTrace();
+                    }
+                    pfd.close();
+                }
+                catch (FileNotFoundException e)
+                {
+                    Log.i("FILEWRITER", e.getMessage());
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Log.i("FILEWRITER", "Wrote File");
+            }
+        }
+
+
     }
 
     public void startDateDialog()
@@ -101,7 +158,6 @@ public class GenerateQR extends AppCompatActivity implements Observer<QrAttempt>
 
     public void saveQrButton() {
         Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show();
-        viewModel.saveEvent(currentEvent);
     }
 
 }
