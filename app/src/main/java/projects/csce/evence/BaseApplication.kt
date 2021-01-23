396c30985bfadb78e2b72776ead999c94dfab110
danielP <doperez1997@yahoo.com>
2021-01-23 16:07:52 -0600

    Changed BaseApplication.java to BaseApplication.kt

--- app/src/main/java/projects/csce/evence/BaseApplication.kt
+++ app/src/main/java/projects/csce/evence/BaseApplication.kt
@@ -1,90 +1,55 @@
-package projects.csce.evence;
+package projects.csce.evence
 
-import android.app.Application;
+import android.app.Application
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import daniel.perez.core.service.FileManager
+import daniel.perez.generateqrview.di.GenerateQRComponent
+import daniel.perez.generateqrview.di.GenerateQRComponentProvider
+import daniel.perez.qrcameraview.di.QrReaderComponent
+import daniel.perez.qrcameraview.di.QrReaderComponentProvider
+import daniel.perez.qrdialogview.di.QRDialogComponent
+import daniel.perez.qrdialogview.di.QRDialogComponentProvider
+import projects.csce.evence.di.appscope.AppComponent
+import projects.csce.evence.di.appscope.DaggerAppComponent
+import projects.csce.evence.di.appscope.LoginModule
+import projects.csce.evence.service.model.SharedPref
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+import javax.inject.Inject
 
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+class BaseApplication : Application(), QrReaderComponentProvider, GenerateQRComponentProvider, QRDialogComponentProvider {
+    lateinit var injector: AppComponent
+    lateinit var account: GoogleSignInAccount
 
-import org.jetbrains.annotations.NotNull;
-
-import javax.inject.Inject;
-
-import daniel.perez.core.service.FileManager;
-import daniel.perez.generateqrview.di.GenerateQRComponent;
-import daniel.perez.generateqrview.di.GenerateQRComponentProvider;
-import daniel.perez.qrcameraview.di.QrReaderComponent;
-import daniel.perez.qrcameraview.di.QrReaderComponentProvider;
-import daniel.perez.qrdialogview.di.QRDialogComponent;
-import daniel.perez.qrdialogview.di.QRDialogComponentProvider;
-import projects.csce.evence.di.appscope.AppComponent;
-import projects.csce.evence.di.appscope.DaggerAppComponent;
-import projects.csce.evence.di.appscope.LoginModule;
-import projects.csce.evence.service.model.SharedPref;
-import timber.log.Timber;
-
-
-public class BaseApplication extends Application implements QrReaderComponentProvider, GenerateQRComponentProvider, QRDialogComponentProvider
-{
-
-    private AppComponent injector;
-    private GoogleSignInAccount account;
-    @Inject FileManager fileManager;
-
-    @Override
-    public void onCreate()
-    {
-        super.onCreate();
+    @JvmField
+    @Inject
+    var fileManager: FileManager? = null
+    override fun onCreate() {
+        super.onCreate()
 
         // Setup Timber
-        if(BuildConfig.DEBUG)
-        {
-            Timber.plant(new Timber.DebugTree());
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
         }
-
         injector = DaggerAppComponent.builder()
-                .loginModule( new LoginModule( getApplicationContext() ) )
-                .build();
-
-        injector.inject(this);
-
-        new SharedPref(getApplicationContext()).setSavedPreferences();
-
+                .loginModule(LoginModule(applicationContext))
+                .build()
+        injector.inject(this)
+        SharedPref(applicationContext).setSavedPreferences()
     }
 
-
-
-    public AppComponent getInjector()
+    override fun getQrReaderComponent(): QrReaderComponent
     {
-        return injector;
+        return injector.provideQrReaderFactory().create()
     }
 
-    public void setAccount(GoogleSignInAccount account)
+    override fun provideGenerateQRComponent(): GenerateQRComponent
     {
-        this.account = account;
+        return injector.provideGenerateQrFactory().create()
     }
 
-    public GoogleSignInAccount getAccount()
+    override fun provideQrDialogComponent(): QRDialogComponent
     {
-        return account;
-    }
-
-    @NotNull
-    @Override
-    public QrReaderComponent getQrReaderComponent()
-    {
-        return injector.provideQrReaderFactory().create();
-    }
-
-    @NotNull
-    @Override
-    public GenerateQRComponent provideGenerateQRComponent()
-    {
-        return injector.provideGenerateQrFactory().create();
-    }
-
-    @NotNull
-    @Override
-    public QRDialogComponent provideQrDialogComponent()
-    {
-        return injector.providerQrDialogFactory().create();
+        return injector.providerQrDialogFactory().create()
     }
 }
