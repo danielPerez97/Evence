14527a3620e8e0f5d570bf038608b378da3461e0
DcGabriel707 <gddelcar@uark.edu>
2021-01-23 16:16:58 -0600

    converted SecondActivity.java to kotlin

--- app/src/main/java/projects/csce/evence/view/ui/SecondActivity.kt
+++ app/src/main/java/projects/csce/evence/view/ui/SecondActivity.kt
@@ -1,31 +1,27 @@
-package projects.csce.evence.view.ui;
+package projects.csce.evence.view.ui
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import projects.csce.evence.BaseApplication
+import projects.csce.evence.databinding.ActivityLoggedInBinding
+import projects.csce.evence.di.loginscope.LoggedInModule
+import projects.csce.evence.utils.getAppComponent
+import javax.inject.Inject
 
-import androidx.appcompat.app.AppCompatActivity;
+class SecondActivity : AppCompatActivity() {
+    private var binding: ActivityLoggedInBinding? = null
 
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-
-import javax.inject.Inject;
-
-import projects.csce.evence.BaseApplication;
-import projects.csce.evence.databinding.ActivityLoggedInBinding;
-import projects.csce.evence.di.loginscope.LoggedInModule;
-import projects.csce.evence.utils.Utils;
-
-public class SecondActivity extends AppCompatActivity
-{
-    private ActivityLoggedInBinding binding;
-    @Inject GoogleSignInAccount account;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Utils.getAppComponent(this)
-                .newLoggedInSubComponent(new LoggedInModule(((BaseApplication) getApplication()).getAccount()))
-                .inject(this);
-        super.onCreate(savedInstanceState);
-        binding = ActivityLoggedInBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-//        binding.setAccount(account);
+    @JvmField
+    @Inject
+    var account: GoogleSignInAccount? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        this.getAppComponent()
+                .newLoggedInSubComponent(LoggedInModule((application as BaseApplication).account))
+                .inject(this)
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoggedInBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        //        binding.setAccount(account);
     }
 }
