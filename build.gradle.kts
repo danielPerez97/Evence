48f5c1a7a87ab157bbfc509be50ae06981c64416
danielP <doperez1997@yahoo.com>
2021-01-29 15:35:47 -0600

    Updated project build.gradle to build.gradle.kts and upgrade to Kotlin 1.4.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.72'
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:4.1.2'
-        classpath 'com.google.gms:google-services:4.3.2'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("com.google.gms:google-services:4.3.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Project.kotlinVersion}")
         
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -24,6 +23,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
