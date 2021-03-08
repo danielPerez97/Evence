5232e1160d1c4e79c226726db2a7eb43286ed8dd
danielP <doperez1997@yahoo.com>
2021-03-07 21:04:32 -0600

    Optimized imports across the entire codebase

--- app/src/main/java/projects/csce/evence/viewmodel/MainViewModelModule.kt
+++ app/src/main/java/projects/csce/evence/viewmodel/MainViewModelModule.kt
@@ -1,12 +1,6 @@
 package projects.csce.evence.viewmodel
 
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
 import daniel.perez.core.db.EventOps
-import javax.inject.Singleton
 
 //@Module
 //@InstallIn(ViewModelComponent::class)
--- app/src/test/java/projects/csce/evence/ExampleUnitTest.java
+++ app/src/test/java/projects/csce/evence/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package projects.csce.evence;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
