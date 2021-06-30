b9b2ee08ff986ebf7e25b85956594101bf2d3de1
DcGabriel707 <gddelcar@uark.edu>
2021-06-30 02:03:19 -0500

    fix time format in qrdialog. hide recurrence feature for now

--- GenerateQRView/src/main/java/daniel/perez/generateqrview/GenerateQR.kt
+++ GenerateQRView/src/main/java/daniel/perez/generateqrview/GenerateQR.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.lifecycle.ViewModelProvider
 import com.jakewharton.rxbinding4.view.clicks
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,16 +16,13 @@ import daniel.perez.core.model.Half
 import daniel.perez.core.model.TimeSetEvent
 import daniel.perez.core.model.ViewEvent
 import daniel.perez.generateqrview.databinding.ActivityGenerateQrBinding
-import daniel.perez.ical.Parser.parse
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import timber.log.Timber
-import java.io.File
 import java.time.LocalDateTime
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class GenerateQR : BaseActivity(), DialogClosable, AdapterView.OnItemSelectedListener
-{
+class GenerateQR : BaseActivity(), DialogClosable, AdapterView.OnItemSelectedListener {
     private lateinit var viewModel: GenerateQrViewModel
     private lateinit var binding: ActivityGenerateQrBinding
     private lateinit var currentEvent: ViewEvent
@@ -34,56 +30,55 @@ class GenerateQR : BaseActivity(), DialogClosable, AdapterView.OnItemSelectedLis
     private var endTime = TimeSetEvent(0, 0, Half.AM)
     private var startDate = DateSetEvent(1, 31, 1999)
     private var endDate = DateSetEvent(1, 31, 1999)
-    @Inject lateinit var dialogStarter: DialogStarter
-    @Inject lateinit var activityResultActions: ActivityResultActions
+    @Inject
+    lateinit var dialogStarter: DialogStarter
+    @Inject
+    lateinit var activityResultActions: ActivityResultActions
     private var isEditing = false
 
     @SuppressLint("CheckResult")
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGenerateQrBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewModel = ViewModelProvider(this).get(GenerateQrViewModel::class.java)
 
-        if (intent != null && intent.getLongExtra("EVENT_ID", -1L) != -1L)
-        {
+        if (intent != null && intent.getLongExtra("EVENT_ID", -1L) != -1L) {
             isEditing = true
             val id = intent.getLongExtra("EVENT_ID", -1)
             disposables += viewModel.getEvent(id)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    currentEvent = it
-                    Timber.i(it.toString())
-                    fillInFields()
-                }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        currentEvent = it
+                        Timber.i(it.toString())
+                        fillInFields()
+                    }
         }
 
+        /** for future updates. to set recurrence
         ArrayAdapter.createFromResource(this, R.array.event_recurrence_options_list, R.layout.spinner_text)
-                .also { adapter ->
-                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                    binding.recurrenceSpinner.adapter = adapter
-                }
+        .also { adapter ->
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.recurrenceSpinner.adapter = adapter
+        }
         binding.recurrenceSpinner.onItemSelectedListener = this
+         */
+
         disposables += binding.startDateEditText.clicks().subscribe { startDateDialog() }
         binding.startTimeEditText.clicks().subscribe { startTimeDialog() }
         binding.endDateEditText.clicks().subscribe { endDateDialog() }
         binding.endTimeEditText.clicks().subscribe { endTimeDialog() }
         binding.finishButton.clicks().subscribe {
-            if(!isEditing) {
+            if (!isEditing) {
                 saveEvent()
-            }
-            else {
+            } else {
                 updateEvent()
             }
         }
     }
 
-
-
     //fill in editText and other fields when event is being edited
-    private fun fillInFields()
-    {
+    private fun fillInFields() {
         val event = currentEvent
 
         //TODO("Implement the enum properly, calculate if its morning or after noon for AM, PM")
@@ -101,7 +96,7 @@ class GenerateQR : BaseActivity(), DialogClosable, AdapterView.OnItemSelectedLis
         binding.descriptionEditText.setText(event.description)
     }
 
-    private fun areValidFields() : Boolean {
+    private fun areValidFields(): Boolean {
         if (binding.titleEditText.text.isBlank()) {
             applicationContext.snackbarShort(binding.titleEditText, getString(R.string.enter_valid_title))
             return false
@@ -130,36 +125,33 @@ class GenerateQR : BaseActivity(), DialogClosable, AdapterView.OnItemSelectedLis
         return true
     }
 
-    private fun saveEvent()
-    {
+    private fun saveEvent() {
         if (!areValidFields()) {
             return
         }
         val uiNewEvent: UiNewEvent = extractEventFromUi()
         disposables += viewModel.saveEvent(uiNewEvent)
-                .observeOn( AndroidSchedulers.mainThread() )
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
                     currentEvent = it.toViewEvent()
                     dialogStarter.startQrDialog(this, it.toViewEvent())
                 }
     }
 
-    private fun updateEvent()
-    {
+    private fun updateEvent() {
         if (!areValidFields()) {
             return
         }
         val uiNewEvent: UiNewEvent = extractEventFromUi()
         disposables += viewModel.updateEvent(currentEvent.id, uiNewEvent)
-            .observeOn( AndroidSchedulers.mainThread() )
-            .subscribe {
-                currentEvent = it.toViewEvent()
-                dialogStarter.startQrDialog(this, it.toViewEvent())
-            }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    currentEvent = it.toViewEvent()
+                    dialogStarter.startQrDialog(this, it.toViewEvent())
+                }
     }
 
-    private fun extractEventFromUi(): UiNewEvent
-    {
+    private fun extractEventFromUi(): UiNewEvent {
         Timber.i("Start_Date: ${startDate.string()}")
         Timber.i("Start_Time: $startTime")
         Timber.i("End_Date: ${endDate.string()}")
@@ -171,7 +163,7 @@ class GenerateQR : BaseActivity(), DialogClosable, AdapterView.OnItemSelectedLis
                     titleEditText.text.toString(),
                     descriptionEditText.text.toString(),
                     locationEditText.text.toString(),
-                    LocalDateTime.parse("${startDate.string()}T${startTime.string()}" ), //todo add method into Utils.kt
+                    LocalDateTime.parse("${startDate.string()}T${startTime.string()}"), //todo add method into Utils.kt
                     LocalDateTime.parse("${endDate.string()}T${endTime.string()}"),
             )
         }
@@ -180,25 +172,19 @@ class GenerateQR : BaseActivity(), DialogClosable, AdapterView.OnItemSelectedLis
     }
 
     // Handle the user choosing a place to store the file
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
-    {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RequestCodes.REQUEST_SAF)
-        {
-            if (data != null)
-            {
+        if (requestCode == RequestCodes.REQUEST_SAF) {
+            if (data != null) {
                 disposables += activityResultActions.actionCreateDocumentEvent(this, currentEvent, data)
                         .subscribe {
-                            when(it)
-                            {
+                            when (it) {
                                 ActionResult.Success -> toastShort("Wrote File Successfully")
-                                is ActionResult.Failure ->
-                                {
+                                is ActionResult.Failure -> {
                                     Timber.e(it.t)
                                     toastShort("Error writing file")
                                 }
-                                ActionResult.InTransit ->
-                                {
+                                ActionResult.InTransit -> {
                                     Timber.i("Writing file...")
                                 }
                             }
@@ -207,8 +193,7 @@ class GenerateQR : BaseActivity(), DialogClosable, AdapterView.OnItemSelectedLis
         }
     }
 
-    private fun startDateDialog()
-    {
+    private fun startDateDialog() {
         disposables.add(dialogStarter.startDateDialog(this)
                 .subscribe {
                     startDate = it
@@ -216,27 +201,24 @@ class GenerateQR : BaseActivity(), DialogClosable, AdapterView.OnItemSelectedLis
                 })
     }
 
-    private fun endDateDialog()
-    {
-        disposables.add( dialogStarter.startDateDialog( this )
+    private fun endDateDialog() {
+        disposables.add(dialogStarter.startDateDialog(this)
                 .subscribe {
                     endDate = it
                     binding.endDateTextView.text = "${it.month}-${it.dayOfMonth}-${it.year}"
 
-                } )
+                })
     }
 
-    private fun startTimeDialog()
-    {
+    private fun startTimeDialog() {
         disposables.add(dialogStarter.startTimeDialog(this)
                 .subscribe {
                     startTime = it
                     binding.startTimeTextView.text = getAMPMTimeFormat("${it.hour} ${it.minute}")
-                } )
+                })
     }
 
-    private fun endTimeDialog()
-    {
+    private fun endTimeDialog() {
         disposables.add(dialogStarter.startTimeDialog(this)
                 .subscribe {
                     endTime = it
@@ -249,8 +231,7 @@ class GenerateQR : BaseActivity(), DialogClosable, AdapterView.OnItemSelectedLis
         disposables.dispose()
     }
 
-    override fun close()
-    {
+    override fun close() {
         finish()
     }
 
