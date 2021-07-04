8e90a9aa0db4271622fce7ee5ec6f51a4aa6e283
danielPerez97 <doperez1997@gmail.com>
2021-07-04 01:19:47 -0500

    Corrected the dark theme of LicensesActivity.kt

--- Core/src/main/java/daniel/perez/core/compose/DarkColors.kt
+++ Core/src/main/java/daniel/perez/core/compose/DarkColors.kt
@@ -3,7 +3,7 @@ package daniel.perez.core.compose
 import androidx.compose.material.darkColors
 import androidx.compose.ui.graphics.Color
 
-private val Primary = Color(android.graphics.Color.parseColor("#2196F3"))
+private val Primary = Color(android.graphics.Color.parseColor("#263238"))
 private val PrimaryVariant = Color(android.graphics.Color.parseColor("#263238"))
 private val OnPrimary = Color.White
 private val Secondary = Color(android.graphics.Color.parseColor("#263238"))
