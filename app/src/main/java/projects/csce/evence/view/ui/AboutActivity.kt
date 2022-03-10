96c552570d8e9dfc2d876581ac63e5f0c2197b4a
DcGabriel707 <gddelcar@uark.edu>
2022-03-10 15:07:30 -0600

    UI changes (#33)
    * switched placement for done and delete button. other smaller ui changes
    
    * switched back placement of close and delete button
    
    * wip added sort button
    
    * created sort button. waiting for database implementation
    
    * Revert "merge"
    
    This reverts commit a68a567575d2621350a1f010aa8b931981badb85, reversing
    changes made to 03d2744f07a8d0e56dbe12ff4cf000be2b7fecdf.
    
    * added privacy policy
    
    * fixed privacy policy
    
    * updated about page. implemented email feedback functionality

--- app/src/main/java/projects/csce/evence/view/ui/AboutActivity.kt
+++ app/src/main/java/projects/csce/evence/view/ui/AboutActivity.kt
@@ -1,8 +1,11 @@
 package projects.csce.evence.view.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Html
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
 import androidx.core.text.HtmlCompat.fromHtml
@@ -14,40 +17,56 @@ import javax.inject.Inject
 
 
 @AndroidEntryPoint
-class AboutActivity : AppCompatActivity()
-{
+class AboutActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityAboutBinding
-    @Inject lateinit var navigator: ActivityStarter
+    @Inject
+    lateinit var navigator: ActivityStarter
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.cardView.setOnClickListener { navigator.startLicenseActivity(this) }
-        binding.privacyPolicyContentText.text =  HtmlCompat.fromHtml(getString(R.string.privacy_policy_content), HtmlCompat.FROM_HTML_MODE_LEGACY)
+        binding.emailButton.setOnClickListener { sendFeedback() }
+        binding.privacyPolicyContentText.text = HtmlCompat.fromHtml(
+            getString(R.string.privacy_policy_content),
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
     }
 
-    fun sendFeedback()
-    {
-        //todo: which email to use?
+    fun sendFeedback() {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            val email = getString(R.string.evence_email)
+            data = Uri.parse("mailto:")
+            intent.type = "text/plain"
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.evence_email)))
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.evence_feedback))
+        }
+
+        try {
+        startActivity(Intent.createChooser(intent, "Select your Email app"))
+        }
+        catch (e: Exception) {
+            Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+        }
     }
 
-    fun leaveRating()
-    {
+    fun leaveRating() {
         //todo: open link to playstore page here
     }
 
-    fun privacyPolicy(view: View){
-        if(binding.privacyPolicyContentText.visibility == View.GONE) {
+    fun privacyPolicy(view: View) {
+        if (binding.privacyPolicyContentText.visibility == View.GONE) {
             binding.privacyPolicyContentText.visibility = View.VISIBLE
             binding.privacyPolicyText.visibility = View.GONE
-        }
-        else {
+        } else {
             binding.privacyPolicyContentText.visibility = View.GONE
             binding.privacyPolicyText.visibility = View.VISIBLE
         }
+    }
+
+    fun licenses(view: View) {
+        navigator.startLicenseActivity(this)
     }
 
 }
